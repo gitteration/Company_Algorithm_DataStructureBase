94157ae4a07affc9a869d36b8c43d86a7e99625c
gitteration <show10033@naver.com>
2023-03-22 23:41:42 +0900

    fix: 회사 계정으로 잘못 푸쉬 되어 다시 푸쉬

--- problem-solving-patterns/sanghwaLee.js
+++ problem-solving-patterns/sanghwaLee.js
@@ -3,10 +3,10 @@ function validAnagram(str1, str2){
     let result1 = 0,result2 = 0;
 
     for(const char of str1){
-        result1 += char.charCodeAt()
+        result1 += char.charCodeAt();
     }
     for(const char of str2){
-        result2 += char.charCodeAt()
+        result2 += char.charCodeAt();
     }
     return result1 === result2;
 }
