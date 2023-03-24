5d99dfd4e40ecfc09842d4720024b00499e7d067
kjuiop <copying_y@naver.com>
2023-03-24 12:25:04 +0900

    feat: 문제 해결 패턴 (divide-conquer)

---
+++ problem-solving-patterns/junginKim/divideConquer/Search.js
@@ -0,0 +1,37 @@
+/**
+ * 문제를 이해하라
+ * 문제를 해결할 바탕이 되는 계획들을 세워라
+ * 작성한 내용들을 다 지우고, 다시 시작해서 계획들을 세워라
+ * 해결 가능한 문제들은 풀고, 불가한 문제들은 단순화시켜라
+ * 회고하고, 리팩토링하라
+ */
+
+
+/**
+ * 오름차순 정렬된 integer 배열이 들어왔을 때, number 로 들어온 parameter 가 배열의 어느 요소에 있는지 구하라
+ *
+ * search([1,2,3,4,5,6],4)  // 3
+ * search([1,2,3,4,5,6], 5 // 5
+ * search([1,2,3,4,5,6], 11 // -1
+ */
+
+function search(arr, num) {
+
+    if (arr.length <= 0) {
+        return -1;
+    }
+
+    for (let i=0; i<arr.length; i++) {
+        if (arr[i] === num) {
+            return i+1;
+        }
+    }
+
+    return -1;
+}
+
+
+
+console.log("maxSubarraySum2 : " + search([1,2,3,4,5,6],4));  // 4
+console.log("maxSubarraySum2 : " + search([1,2,3,4,5,6], 5));  // 5
+console.log("maxSubarraySum2 : " + search([1,2,3,4,5,6], 11)); // -1
