8594b91b3a9fd2ec4b5435cc81ed61721d5112bd
kjuiop <copying_y@naver.com>
2023-05-12 06:54:05 +0900

    7주차 삽입정렬

---
+++ insertion-alignment/jungunKim.js
@@ -0,0 +1,14 @@
+function insertionSort(arr) {
+
+    for (let i=1; i<arr.length; i++) {
+        let currentVal = arr[i];
+        for (let j=i-1; j>=0 && arr[j] > currentVal; j--) {
+            arr[j+1] = arr[j]
+        }
+        arr[j+1] = currentVal
+        console.log(arr)
+    }
+    return arr;
+}
+
+console.log("result : ", insertionSort([2, 1, 9, 76, 4]))
