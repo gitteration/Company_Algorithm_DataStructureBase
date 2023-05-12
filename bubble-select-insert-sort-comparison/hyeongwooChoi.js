c1884faa5f8e3da4b0ccec3ddf3c1dfce940305c
94-c <hyeongwoo26@gmail.com>
2023-05-12 10:34:10 +0900

    # 7회차 - 버블, 정렬 코드 수정

--- bubble-select-insert-sort-comparison/hyeongwooChoi.js
+++ bubble-select-insert-sort-comparison/hyeongwooChoi.js
@@ -84,16 +84,3 @@ function selectionSortRe(arr) {
 
 console.log("selection Sort Re : ", selectionSortRe([12, 88, 56, 14, 1]));
 
-
-function insertionSort(arr) {
-
-    for (let i = 1; i < arr.length; i++) {
-        let currentVal = arr[i];
-        for (let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
-            arr[j + 1] = arr[j]
-        }
-    }
-    return arr;
-}
-
-console.log("insertion sort : ", insertionSort([8, 2, 1, 99, 34]))
