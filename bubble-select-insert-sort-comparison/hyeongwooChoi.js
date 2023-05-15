68183106912c2612b865a27cc58dda124583bb87
HyeongWooChoi <hw.choi@catenoid.net>
2023-05-15 18:36:08 +0900

    # 7회차 - 섹션 문제 풀이 - 43번

--- bubble-select-insert-sort-comparison/hyeongwooChoi.js
+++ bubble-select-insert-sort-comparison/hyeongwooChoi.js
@@ -84,3 +84,35 @@ function selectionSortRe(arr) {
 
 console.log("selection Sort Re : ", selectionSortRe([12, 88, 56, 14, 1]));
 
+//--------------------------------------------------------- 연습 문제  ---------------------------------------------------------
+/**
+ * 섹션 30 : 더 와이들 웨스트
+ * 코딩 연습 43 : Bubble Sort
+ */
+function bubbleSort_test(arr, comparator) {
+    // comparator가 함수인지 확인하고, 그렇지 않으면 기본 comparator를 제공합니다.
+    if (typeof comparator !== 'function') {
+        comparator = function(a, b) {
+            return a - b; // 기본 comparator: 오름차순
+        };
+    }
+
+    // Bubble sort 알고리즘
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            // 제공된 comparator를 사용하여 인접한 요소들을 비교합니다.
+            if (comparator(arr[j], arr[j + 1]) > 0) {
+                // 요소가 잘못된 순서에 있다면 교환합니다.
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    // 정렬된 배열을 반환합니다.
+    return arr;
+}
+console.log(bubbleSort_test([4, 20, 12, 10, 7, 9])); // [4, 7, 9, 10, 12, 20]
+console.log(bubbleSort_test([0, -10, 7, 4])); // [-10, 0, 4, 7]
+console.log(bubbleSort_test([1, 2, 3])); // [1, 2, 3]
