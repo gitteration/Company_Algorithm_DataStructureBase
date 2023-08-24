61c09f31b2833e191a8d41c8a74e0059d9908d2c
HyeongWooChoi <hw.choi@catenoid.net>
2023-08-24 11:28:27 +0900

    # 26.그래프 - 선(Edge) 제거 솔루션

--- 26-graph/hyeongwooChoi.js
+++ 26-graph/hyeongwooChoi.js
@@ -22,6 +22,18 @@ class Graph{
         this.adjacencyList[v1].push(v2);
         this.adjacencyList[v2].push(v1 );
     }
+
+    /**
+     * 선(Edge) 제거 솔루션
+     */
+    removeEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+            v => v !== vertex2
+        );
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+            v => v !== vertex1
+        );
+    }
 }
 
 let g = new Graph();
@@ -29,4 +41,3 @@ g.addVertex("Dallas");
 g.addVertex("Seoul");
 g.addVertex("Inchon");
 
-console.log(g.addEdge("Dallas", "Seoul"));
