68bb9c32173e89e4bd239279d099398775ea0989
HyeongWooChoi <hw.choi@catenoid.net>
2023-08-24 11:21:15 +0900

    # 26.그래프 - 점(vertex) 추가 솔루션

---
+++ 26-graph/hyeongwooChoi.js
@@ -0,0 +1,13 @@
+/**
+ * 그래프의 유형
+ */
+
+class Graph{
+    constructor() {
+        this.adjacencyList = {};
+    }
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    }
+
+}
