07b42f5406ca7f6d5673a1d45b4b3c788b7cf36e
HyeongWooChoi <hw.choi@catenoid.net>
2023-08-24 11:26:34 +0900

    # 26.그래프 - 선(Edge) 추가 솔루션

--- 26-graph/hyeongwooChoi.js
+++ 26-graph/hyeongwooChoi.js
@@ -6,8 +6,27 @@ class Graph{
     constructor() {
         this.adjacencyList = {};
     }
+
+    /**
+     * 점(vertex)
+     *
+     */
     addVertex(vertex) {
         if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
     }
 
+    /**
+     * 선(Edge)
+     */
+    addEdge(v1, v2) {
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1 );
+    }
 }
+
+let g = new Graph();
+g.addVertex("Dallas");
+g.addVertex("Seoul");
+g.addVertex("Inchon");
+
+console.log(g.addEdge("Dallas", "Seoul"));
